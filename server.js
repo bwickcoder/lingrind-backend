e950148ae4677f742e56b0462e7a0448365f37c1
Brenten Wick <brentwicken@gmail.com>
2025-06-25 19:45:20 -0500

    Syncing cards

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
+import userFlashcardRoutes from "./routes/userFlashcards.js";
+
 
 
 dotenv.config();
@@ -63,6 +65,7 @@ app.use(cors({
 
 
 
+app.use("/api/userFlashcards", userFlashcardRoutes);
 
 
 
